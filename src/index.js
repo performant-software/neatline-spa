63dc8804d400c6df79dec781fbd527078c7c8624
Andrew Sempere <a@feralresearch.org>
2018-07-13 01:09:38 +0200

    Fixing CSS to build in with app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from './store';
 import App from './views/app';
+import styles from './main.css';
 // import registerServiceWorker from './registerServiceWorker';
 
 // registerServiceWorker();
